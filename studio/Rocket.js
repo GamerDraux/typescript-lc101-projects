99ccd7929d1edf0580d7f3a8ca7f94a1997d54b4
gamerDraux <61572578+GamerDraux@users.noreply.github.com>
2020-03-23 20:29:27 -0500

    dead code

--- studio/Rocket.js
+++ studio/Rocket.js
@@ -2,22 +2,26 @@
 exports.__esModule = true;
 var Rocket = /** @class */ (function () {
     function Rocket(name, totalCapacityKg) {
+        constructor (name, totalCapacityKg){
         this.name = name;
         this.totalCapacityKg = totalCapacityKg;
+        this.cargoItems=Cargo[];
+        this.astronauts= Astronaut[];
+        this.name=name;
+        this.totalCapacityKg=totalCapacityKg;
     }
     Rocket.prototype.sumMass = function (items) {
         // returns sum of all items using each item's massKg property
         var categoryMass = 0;
-        for (var i = 0; i < items.length; i++) {
-            categoryMass += items[i].massKg;
+        for (let item of items) {
+            categoryMass += item.massKg;
         }
         return categoryMass;
     };
     Rocket.prototype.currentMassKg = function () {
         //create current Mass calculation here
         // uses this.sumMass to return the combined mass of this.astronauts and this.cargoItems
-        var totalMass = this.sumMass(this.cargoItems) + this.sumMass(this.astronauts);
-        return totalMass;
+        return this.sumMass(this.cargoItems) + this.sumMass(this.astronauts);
     };
     Rocket.prototype.canAdd = function (item) {
         // returns true if this.currentMassKg() + item.massKg <= this.totalCapacityKg
@@ -30,7 +34,7 @@ var Rocket = /** @class */ (function () {
     };
     Rocket.prototype.addCargo = function (cargo) {
         //uses this.canAdd() to see if another item can be added, if true, adds CARGO to THIS.CARGOITEMS and returns true, else, returns false
-        if (this.canAdd(cargo) === true) {
+        if (this.canAdd(cargo)) {
             this.cargoItems.push(cargo);
             return true;
         }
--- studio/rocket.ts
+++ studio/rocket.ts
@@ -1,4 +1,4 @@
-import {Payload} from './Payload';
+import {Payload} from './Payload'
 import {Cargo} from './Cargo';
 import {Astronaut} from './Astronaut';
 
@@ -13,9 +13,9 @@ export class Rocket {
     }
     sumMass(items:Payload[]):number{
         // returns sum of all items using each item's massKg property
-        let categoryMass:number=0;
-        for (let i = 0; i<items.length; i++){
-            categoryMass += items[i].massKg;
+        let categoryMass=0;
+        for (let item of items){
+            categoryMass += item.massKg;
         }
         return categoryMass;
     }
@@ -23,8 +23,7 @@ export class Rocket {
     currentMassKg():number {
         //create current Mass calculation here
         // uses this.sumMass to return the combined mass of this.astronauts and this.cargoItems
-        let totalMass = this.sumMass(this.cargoItems)+this.sumMass(this.astronauts);
-        return totalMass;
+         return this.sumMass(this.cargoItems)+this.sumMass(this.astronauts);
     }
     canAdd (item:Payload):boolean {
         // returns true if this.currentMassKg() + item.massKg <= this.totalCapacityKg
@@ -34,16 +33,17 @@ export class Rocket {
             return false;
         }
     }
-    addCargo(cargo: Cargo){
+
+    addCargo(cargo: Cargo):boolean{
         //uses this.canAdd() to see if another item can be added, if true, adds CARGO to THIS.CARGOITEMS and returns true, else, returns false
-        if (this.canAdd(cargo)===true){
+        if (this.canAdd(cargo)){
             this.cargoItems.push(cargo);
             return true;
         } else {
             return false;
         }
     }
-    addAstronaut(astronaut: Astronaut){
+    addAstronaut(astronaut: Astronaut):boolean{
         //uses this.canAdd() to see if another astronaut can be added, if true, adds astronuat to this.astronauts and returns true, else returns false
         if (this.canAdd(astronaut)===true){
             this.astronauts.push(astronaut);
